d21b239f970f9b2f23980602579343a077a4a4ca
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-23 06:29:38 +0300

    Add homescreen and productscreen components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -21,14 +21,7 @@ function App() {
     <main>
       <Route path="/product/:id" component={ProductScreen} ></Route>
       <Route path="/" component={HomeScreen} exact ></Route>
-      <div className="row center">
-        {
-          data.products.map(product => (
-            <Product key={product._id} product={product}></Product>
-          ))
-        }
-        
-      </div>
+    
     </main>
 
     <footer className="row center">
