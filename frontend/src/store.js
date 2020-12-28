6b1f961b0ab87a2b4964d034e8708c2753dcd99d
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-29 02:04:26 +0300

    Add redux thunk to the app

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import data from './data';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 const initialState = {};
 const reducer = (state, action) => {
   return {products: data.products}
 }
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
 
 export default store;
